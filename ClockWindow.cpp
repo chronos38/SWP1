88605dd14d096c1b3d689883080b12126fd8576f
David Wolf <d.wolf@live.at>
2014-03-05 12:17:56 +0100

    OpenGL-Window

--- ClockWindow.cpp
+++ ClockWindow.cpp
@@ -107,7 +107,7 @@ bool ClockWindow::CreateWindow(const char* title, int x, int y)
 				   y == -1 ? SDL_WINDOWPOS_UNDEFINED : y,
 				   WINDOW_WIDTH,
 				   WINDOW_HEIGHT,
-				   SDL_WINDOW_SHOWN);
+				   SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
 
 	// check if window is a nullptr
 	if (!mWindow) {
