e67e801efed296fbe1702a6497e76861f840e95e
Leonhardt Schwarz <if12b076@technikum-wien.at>
2014-05-15 22:49:18 +0200

    Clock Attach & Dettach Tests

--- Tests/ClockTest.cs
+++ Tests/ClockTest.cs
@@ -3,6 +3,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.QualityTools.Testing.Fakes;
 using System.Fakes;
 using SWP1;
+using System.Threading;
+using Tests.Mock;
 
 namespace Tests
 {
@@ -18,9 +20,8 @@ namespace Tests
 			_clock = Clock.Instance;
 			_shimCon = ShimsContext.Create();
 			//Shim to set Time always to 0:0:0
-			ShimDateTime.UtcNowGet = () =>
-				{
-					return new DateTime(2000,1,1,0,0,0);
+			ShimDateTime.UtcNowGet = () => {
+					return new DateTime(2000, 1, 1, 0, 0, 0);
 				};
 		}
 
@@ -60,7 +61,7 @@ namespace Tests
 			_clock.Set(0, 0, 61);
 			Assert.AreEqual(1, _clock.Second);
 		}
-		
+
 		[TestMethod]
 		public void IncDecIsProperlyRollingOverWhenIncrementing()
 		{
@@ -69,33 +70,31 @@ namespace Tests
 
 			//seconds rollover
 			for (int i = 0; i < 120; i++) {
-				_clock.IncDec(0,0,1);
+				_clock.IncDec(0, 0, 1);
 			}
 			Assert.AreEqual(0, _clock.Hour);
 			Assert.AreEqual(2, _clock.Minute);
 			Assert.AreEqual(0, _clock.Second);
 
 			_clock.Set(0, 0, 0);
-			
+
 			//minutes rollover
 			for (int i = 0; i < 120; i++) {
-				_clock.IncDec(0,1,0);
+				_clock.IncDec(0, 1, 0);
 			}
 			Assert.AreEqual(2, _clock.Hour);
 			Assert.AreEqual(0, _clock.Minute);
 			Assert.AreEqual(0, _clock.Second);
 
 			_clock.Set(0, 0, 0);
-			
+
 			//hours rollover
 			for (int i = 0; i < 24; i++) {
-				_clock.IncDec(1,0,0);
+				_clock.IncDec(1, 0, 0);
 			}
 			Assert.AreEqual(0, _clock.Hour);
 			Assert.AreEqual(0, _clock.Minute);
 			Assert.AreEqual(0, _clock.Second);
-
-			_clock.Set(0, 0, 0);
 		}
 
 		[TestMethod]
@@ -106,34 +105,93 @@ namespace Tests
 
 			//seconds rollover
 			for (int i = 0; i < 120; i++) {
-				_clock.IncDec(0,0,-1);
+				_clock.IncDec(0, 0, -1);
 			}
 			Assert.AreEqual(23, _clock.Hour);
 			Assert.AreEqual(58, _clock.Minute);
 			Assert.AreEqual(0, _clock.Second);
 
 			_clock.Set(0, 0, 0);
-			
+
 			//minutes rollover
 			for (int i = 0; i < 120; i++) {
-				_clock.IncDec(0,-1,0);
+				_clock.IncDec(0, -1, 0);
 			}
 			Assert.AreEqual(22, _clock.Hour);
 			Assert.AreEqual(0, _clock.Minute);
 			Assert.AreEqual(0, _clock.Second);
 
 			_clock.Set(0, 0, 0);
-			
+
 			//hours rollover
 			for (int i = 0; i < 24; i++) {
-				_clock.IncDec(-1,0,0);
+				_clock.IncDec(-1, 0, 0);
 			}
 			Assert.AreEqual(0, _clock.Hour);
 			Assert.AreEqual(0, _clock.Minute);
 			Assert.AreEqual(0, _clock.Second);
-
-			_clock.Set(0, 0, 0);
 		}
 
+		[TestMethod]
+		[ExpectedException(typeof(ApplicationException))]
+		public void DetachThrowsExceptionWhenDetachingUnknownObserver()
+		{
+			MockOberserver observer = new Mock.MockOberserver();
+			_clock.Detach(observer);
+		}
+
+		[TestMethod]
+		public void ObserverGetsUpdatedByIncDecAndSet()
+		{
+			MockOberserver observer = new Mock.MockOberserver();
+			try {
+				_clock.Attach(observer);
+				_clock.IncDec(1, 0, 0);
+				Assert.IsTrue(ObserverUpdated(observer));
+				observer.UpdateOccured = false;
+				_clock.Set(1, 2, 3);
+				Assert.IsTrue(ObserverUpdated(observer));
+			} finally {
+				_clock.Detach(observer);
+			}
+
+		}
+
+		[TestMethod]
+		public void OberserverGetsUpdatedByTimer()
+		{
+			_clock.Start();
+			MockOberserver observer = new Mock.MockOberserver();
+			_clock.Attach(observer);
+
+			try {
+
+				Assert.IsTrue(ObserverUpdated(observer));
+
+				observer.UpdateOccured = false;
+				_clock.Stop();
+
+				Assert.IsFalse(ObserverUpdated(observer));
+
+			} finally {
+				_clock.Detach(observer);
+				_clock.Stop();
+			}
+		}
+
+		private bool ObserverUpdated(MockOberserver observer)
+		{
+			bool ret = false;
+			AutoResetEvent evt = new AutoResetEvent(false);
+			Timer t = new Timer(state => {
+				while (!observer.UpdateOccured) ;
+				evt.Set();
+			}, null, 1000, Timeout.Infinite);
+
+			if (evt.WaitOne(2000)) {
+				ret = true;
+			}
+			return ret;
+		}
 	}
 }
---
+++ Tests/Mock/MockOberserver.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SWP1;
+
+namespace Tests.Mock
+{
+	class MockOberserver : IObserver
+	{
+
+		public bool UpdateOccured { get; set; }
+		public void Update(ISubject subject)
+		{
+			UpdateOccured = true;
+		}
+	}
+}
