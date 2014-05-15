d4a6638639886af03ba0ca639d2121fded0454b9
Leonhardt Schwarz <if12b076@technikum-wien.at>
2014-05-15 23:15:00 +0200

    EventArgs "Tests"...

---
+++ Tests/ClockEventArgsTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SWP1;
+
+namespace Tests
+{
+	[TestClass]
+	public class ClockEventArgsTest
+	{
+		[TestMethod]
+		public void ClockEventArgsContructorCreatesCorrectValues()
+		{
+			ClockEventArgs args = new ClockEventArgs();
+			Assert.AreEqual(0, args.Hour);
+			Assert.AreEqual(0, args.Minute);
+			Assert.AreEqual(0, args.Second);
+
+			args = new ClockEventArgs(1, 2, 3);
+			Assert.AreEqual(1, args.Hour);
+			Assert.AreEqual(2, args.Minute);
+			Assert.AreEqual(3, args.Second);
+		}
+
+		[TestMethod]
+		public void ClockEventArgsAttributesAreSetable()
+		{
+			ClockEventArgs args = new ClockEventArgs();
+			args.Hour = int.MaxValue;
+			Assert.AreEqual(int.MaxValue, args.Hour);
+			args.Minute = int.MaxValue;
+			Assert.AreEqual(int.MaxValue, args.Minute);
+			args.Second = int.MaxValue;
+			Assert.AreEqual(int.MaxValue, args.Second);
+		}
+	}
+}
---
+++ Tests/ShowEventArgsTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SWP1;
+
+namespace Tests
+{
+	[TestClass]
+	public class ShowEventArgsTest
+	{
+		[TestMethod]
+		public void ShowEventArgsContructorCreatesCorrectValues()
+		{
+			ShowEventArgs args = new ShowEventArgs();
+			Assert.AreEqual(null, args.Timezone);
+			Assert.AreEqual(0, args.X);
+			Assert.AreEqual(0, args.Y);
+			Assert.AreEqual(null, args.Type);
+		}
+
+		[TestMethod]
+		public void ShowEventArgsAttributesAreSetable()
+		{
+			ShowEventArgs args = new ShowEventArgs();
+			args.Type = "test";
+			Assert.AreEqual("test", args.Type);
+			args.X = 1;
+			Assert.AreEqual(1, args.X);
+			args.Y = 1;
+			Assert.AreEqual(1, args.Y);
+			args.Timezone = TimeZoneInfo.Local;
+			Assert.AreEqual(TimeZoneInfo.Local, args.Timezone);
+
+		}
+	}
+}
